55d24597e3c61ff74d7760f1814e2e77148f3c85
Raül Torralba Adsuara <raul.torralba@artansoft.com>
2021-12-19 14:52:03 +0100

    Added utils mappy and ponedor

---
+++ utils/Mappy-mojono/mapa.h
@@ -0,0 +1,16 @@
+// Mapa.h 
+// Generado por MapCnv de la churrera
+// Copyleft 2010 The Mojon Twins
+ 
+unsigned char mapa [] = {
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 102, 102, 102, 102, 102, 102, 102, 96, 0, 0, 0, 0, 0, 0, 0
+};
+ 
+#define MAX_CERROJOS 0
+ 
+typedef struct {
+    unsigned char np, x, y, st;
+} CERROJOS;
+ 
+CERROJOS *cerrojos;
+ 
